cd81befaad62c76bd2a77e7d4abacdbf6b8bc105
Jonathan Hartman <j@hartman.io>
2018-10-09 19:45:57 -0700

    Put quotes around the pkg_source

--- plan.sh
+++ plan.sh
@@ -5,8 +5,7 @@ pkg_version=2.1.26
 pkg_maintainer="Jonathan Hartman <j@hartman.io>"
 pkg_license=(Apache-2.0)
 pkg_upstream_url=https://github.com/RoboticCheese/libsasl2-habitat
-
-pkg_source=ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-${pkg_version}.tar.gz
+pkg_source="ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-${pkg_version}.tar.gz"
 pkg_shasum=8fbc5136512b59bb793657f36fadda6359cae3b08f01fd16b3d406f1345b7bc3
 pkg_dirname=cyrus-sasl-${pkg_version}
 
